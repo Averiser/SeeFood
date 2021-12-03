2619b4da8d3a3a818a37e1f8357c15f16737fa7b
Averiser <averiser@gmail.com>
2021-12-04 00:16:32 +0300

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by MyMacBook on 04.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
